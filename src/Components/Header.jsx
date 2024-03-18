744c580def53f451d753fa7620440584e78a0de8
Nikolay Matsela <aergame333@gmail.com>
2024-03-18 17:16:39 +0200

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,27 +4,31 @@ import logo from './Vector.png'
 function Header() {
     return (
       <header className={style.Header}>
+
         <div className={style.HeaderContainer}>
-            <div className={style.test}>
+
+            <div className={style.CenterLogo}>
                 <img src={logo} alt="Logo" />
-                <span>Develop</span>
+                <span className={style.TextLogo}>Develop</span>
             </div>
 
             <div>
-                <a href=" " className={style.HeaderLink}>About</a>
-                <a href=" " className={style.HeaderLink}>Services</a>
-                <a href=" " className={style.HeaderLink}>Case Studies</a>
-                <a href=" " className={style.HeaderLink}>Blog</a>
-                <a href=" " className={style.HeaderLink}>How it works</a>
-                <a href=" " className={style.HeaderLink}>Hire</a>
+                <a href=" " className={style.HeaderLinks}>About</a>
+                <a href=" " className={style.HeaderLinks}>Services</a>
+                <a href=" " className={style.HeaderLinks}>Case Studies</a>
+                <a href=" " className={style.HeaderLinks}>Blog</a>
+                <a href=" " className={style.HeaderLinks}>How it works</a>
+                <a href=" " className={style.HeaderLinks}>Hire</a>
             </div>
 
-            <div className={style.test}>
+            <div>
                 <button className={style.ButtonContact}>
                     Contact us
                 </button>
             </div>
+
         </div>
+        
       </header>
     );
   }
